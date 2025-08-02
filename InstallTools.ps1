2ff41b2efe06cc0726c0f2c45de2fcc645843d5a
lab1702 <lab1702@gmail.com>
2025-08-02 16:29:46 -0400

    Update InstallTools.ps1

--- InstallTools.ps1
+++ InstallTools.ps1
@@ -1,31 +1,49 @@
-winget install `
-  devcom.jetbrainsmononerdfont `
-  microsoft.visualstudiocode `
-  helix.helix `
-  neovim.neovim `
-  discord.discord `
-  mega.megasync `
-  autohotkey.autohotkey `
-  valve.steam `
-  git.git `
-  github.cli `
-  junegunn.fzf `
-  burntsushi.ripgrep.msvc `
-  waterlan.dos2unix `
-  jqlang.jq `
-  sst.opencode `
-  warp.warp `
-  rproject.r `
-  posit.quarto `
-  johnmacfarlane.pandoc `
-  microsoft.visualstudio.2022.buildtools `
-  python.python.3.13 `
-  astral-sh.uv `
-  astral-sh.ruff `
-  duckdb.cli `
-  golang.go `
-  rustlang.rustup `
-  openjs.nodejs.lts
 
+# Base
+winget install devcom.jetbrainsmononerdfont
+winget install microsoft.visualstudiocode
+winget install helix.helix
+winget install neovim.neovim
+winget install discord.discord
+winget install mega.megasync
+winget install autohotkey.autohotkey
 
+# Steam
+winget install valve.steam
 
+# Dev
+winget install git.git
+winget install github.cli
+winget install junegunn.fzf
+winget install burntsushi.ripgrep.msvc
+winget install waterlan.dos2unix
+winget install jqlang.jq
+
+# AI
+winget install sst.opencode
+winget install warp.warp
+
+# R
+winget install rproject.r
+winget install posit.quarto
+winget install johnmacfarlane.pandoc
+
+# C/C++
+winget install microsoft.visualstudio.2022.buildtools
+
+# Python
+winget install python.python.3.13
+winget install astral-sh.uv
+winget install astral-sh.ruff
+
+# DuckDB
+winget install duckdb.cli
+
+# Go
+winget install golang.go
+
+# Rust
+winget install rustlang.rustup
+
+# NodeJS
+winget install openjs.nodejs.lts
