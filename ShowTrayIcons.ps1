31ef1fcf32d7761f9d8ac30b1f5209a56cd23ff2
lab1702 <lab1702@gmail.com>
2025-08-19 11:11:25 -0400

    Delete ShowTrayIcons.ps1

--- ShowTrayIcons.ps1
+++
@@ -1,4 +0,0 @@
-$RegistryPath = 'HKCU:\Control Panel\NotifyIconSettings'
-$Name = 'IsPromoted'
-$Value = '1'
-Get-ChildItem -path $RegistryPath -Recurse | ForEach-Object {New-ItemProperty -Path $_.PSPath -Name $Name -Value $Value -PropertyType DWORD -Force }
